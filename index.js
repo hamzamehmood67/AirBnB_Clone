effa06f966bf24f85e13c0e23c1ee6d3aed60579
HamzaMehmood <thehamzamehmood@gmail.com>
2024-03-06 00:14:37 +0500

    New COmmit

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Listing = require("./models/listing");
 const path = require(`path`);
+const methodOverride = require(`method-override`);
 const app = express();
 
 app.set(`view engine`, `ejs`);
 app.set(`views`, path.join(__dirname, `views`));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride(`_method`));
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
@@ -35,11 +37,32 @@ app.get("/listings/:id", async (req, res) => {
   res.render("./listings/show.ejs", { list: listing });
 });
 
-app.post(`/listings`, (req, res) => {
-  let listing = req.body;
-  console.log(listing);
-  res.send(`Helo`);
+app.post(`/listings`, async (req, res) => {
+  let listing = req.body.lisitng;
+  let newlisting = new Listing(listing);
+  await newlisting.save();
+  res.redirect(`/listings`);
 });
+
+app.get(`/listings/:id/edit`, async (req, res) => {
+  let { id } = req.params;
+  let list = await Listing.findById(id);
+
+  res.render(`./listings/edit.ejs`, { list: list });
+});
+
+app.put(`/listings/:id`, async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.lisitng });
+  res.redirect(`/listings/${id}`);
+});
+
+app.delete(`/listings/:id`, async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  res.redirect(`/listings`);
+});
+
 app.listen(8080, () => {
   console.log("Server is listening at 8080");
 });
