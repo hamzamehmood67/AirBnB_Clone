159e708c697b6672c2e2e591c0f8a7361b9e6ec9
HamzaMehmood <thehamzamehmood@gmail.com>
2024-03-17 16:50:14 +0500

    Addning Error Handling Class and throw custom errors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Listing = require("./models/listing");
-const path = require(`path`);
-const methodOverride = require(`method-override`);
-const ejsMate = require(`ejs-mate`);
+const Listing = require("./models/listing"); // Database models
+const path = require(`path`); //to join path of static files
+const methodOverride = require(`method-override`); //to send put post patch request from form
+const ejsMate = require(`ejs-mate`); //To make layouts boilerplate in ejs
+const wrapAsync = require(`./utils/wrapAsync.js`); //to handle custom errors...
+const ExpressError = require(`./utils/ExpressError.js`); //to handle custom errors...
 const app = express();
 
 app.set(`view engine`, `ejs`);
@@ -17,53 +19,80 @@ async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
   console.log("Connected Successfully to database");
 }
-
 main().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Runing");
 });
 
-app.get(`/listings`, async (req, res) => {
-  const allListings = await Listing.find({});
-  res.render(`./listings/index.ejs`, { allListings });
-});
+app.get(
+  `/listings`,
+  wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render(`./listings/index.ejs`, { allListings });
+  })
+);
 
 app.get(`/listings/new`, (req, res) => {
   res.render(`./listings/newListing.ejs`);
 });
 
-app.get("/listings/:id", async (req, res) => {
-  const { id } = req.params;
-  const listing = await Listing.findById(id);
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
 
-  res.render("./listings/show.ejs", { list: listing });
+    res.render("./listings/show.ejs", { list: listing });
+  })
+);
+
+app.post(
+  `/listings`,
+  wrapAsync(async (req, res) => {
+    if (!req.body.lisitng)
+      throw new ExpressError(400, `Listing Object is not found`); // If req send from postman and body has no listing
+    let listing = req.body.lisitng;
+    let newlisting = new Listing(listing);
+    await newlisting.save();
+    res.redirect(`/listings`);
+  })
+);
+
+app.get(
+  `/listings/:id/edit`,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let list = await Listing.findById(id);
+
+    res.render(`./listings/edit.ejs`, { list: list });
+  })
+);
+
+app.put(
+  `/listings/:id`,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.lisitng });
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+app.delete(
+  `/listings/:id`,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect(`/listings`);
+  })
+);
+app.all(`*`, (req, res, next) => {
+  next(new ExpressError(`404`, `Page Not found`));
 });
 
-app.post(`/listings`, async (req, res) => {
-  let listing = req.body.lisitng;
-  let newlisting = new Listing(listing);
-  await newlisting.save();
-  res.redirect(`/listings`);
-});
-
-app.get(`/listings/:id/edit`, async (req, res) => {
-  let { id } = req.params;
-  let list = await Listing.findById(id);
-
-  res.render(`./listings/edit.ejs`, { list: list });
-});
-
-app.put(`/listings/:id`, async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.lisitng });
-  res.redirect(`/listings/${id}`);
-});
-
-app.delete(`/listings/:id`, async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  res.redirect(`/listings`);
+app.use((err, req, res, next) => {
+  let { status = 500, message } = err;
+  res.status(status).render(`./listings/Error`, { message });
 });
 
 app.listen(8080, () => {
