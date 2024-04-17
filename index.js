40ff1437a1aaf180f0430e494972511525e82b15
Hamza Mehmood <thehamzamehmood@gmail.com>
2024-04-17 17:08:18 +0500

    adding express session in this project

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const path = require(`path`); //to join path of static files
 const methodOverride = require(`method-override`); //to send put post patch request from form
 const ejsMate = require(`ejs-mate`); //To make layouts boilerplate in ejs
 const ExpressError = require(`./utils/ExpressError.js`); //to handle custom errors...
-
+const session = require(`express-session`);
 
 const app = express();
 
+const sessionOptions={
+  resave: false,
+  secret: "myKey",
+  saveUninitialized: true,
+  cookie : {
+    expires: Date.now() + 7* 24 * 60 * 60 * 1000,
+    maxAge: 7* 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  }
+}
 
+app.use(session(sessionOptions));
 const listings = require("./routes/listings.js");
 const review = require("./routes/review.js");
 
@@ -19,6 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride(`_method`));
 app.use(express.static(path.join(__dirname, `/public`)));
 app.engine(`ejs`, ejsMate);
+
 
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/airbnb");
