d5c2fc444443aefa090ee58ee497a3f340d927b7
HamzaMehmood <thehamzamehmood@gmail.com>
2024-06-03 20:42:14 +0500

    New Changes in Review.js

--- routes/review.js
+++ routes/review.js
@@ -1,19 +1,27 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-const review = require(`../models/review.js`);  ///requiring the review models for storing review
+const review = require(`../models/review.js`); ///requiring the review models for storing review
 const wrapAsync = require(`../utils/wrapAsync.js`); //to handle custom errors...
 const ExpressError = require(`../utils/ExpressError.js`); //to handle custom errors...
 const Listing = require("../models/listing"); // Database models
-const {isValid, isReviewOwner, validateReview}= require("../middlewares.js")  //To check user is Logged In and review is 
+const { isValid, isReviewOwner, validateReview } = require("../middlewares.js"); //To check user is Logged In and review is
 
-const reviewController= require("../controller/reviews.js");
-
-
+const reviewController = require("../controller/reviews.js");
 
 //reviewss
-router.post('/', isValid, validateReview, wrapAsync(reviewController.addReview));
+router.post(
+  "/",
+  isValid,
+  validateReview,
+  wrapAsync(reviewController.addReview)
+);
 
-router.delete('/:reviewId', isReviewOwner, wrapAsync(reviewController.deleteReview))
+router.delete(
+  "/:reviewId",
+  isValid,
+  isReviewOwner,
+  wrapAsync(reviewController.deleteReview)
+);
 
 module.exports = router;
